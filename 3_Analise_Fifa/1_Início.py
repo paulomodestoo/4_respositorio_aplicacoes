3f04b6318f4c3f233dd1b6efb3cc86dc55e0afea
Paulo Modesto <paaulomodesto@gmail.com>
2024-03-23 14:39:48 -0300

    atualizando 1_Início.py

--- 3_Analise_Fifa/1_Início.py
+++ 3_Analise_Fifa/1_Início.py
@@ -14,7 +14,7 @@ st.set_page_config(page_title='Fifa', page_icon=favicon, layout="wide")
 
 @st.cache_data
 def carregar_dados():
-    data_base = pd.read_csv('datasets/CLEAN_FIFA23_official_data.csv')
+    data_base = pd.read_csv('4_repositorio_aplicacoes\3_Analise_Fifa\datasets\CLEAN_FIFA23_official_data.csv')
     return data_base
 
 
